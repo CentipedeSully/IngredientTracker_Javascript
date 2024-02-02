695e89cc34b7d9c83b50c87d74ee954547dd9b60
Will Smith <sullivansmith057@gmail.com>
2024-02-02 14:14:15 -0600

    Implemented chemical field add/remove functionality. Started working on changing the chem fields based on the current query context. Implemented query Context state tracking. Need to debug preexisting chem fields not properly updating when query state changes

--- script.js
+++ script.js
@@ -1,38 +1,54 @@
 //Initial Declarations
-let ingredientCollection = Map.set(); //Key === ingredient name, value === chemical map
+let ingredientCollection = new Map(); //Key === ingredient name, value === chemical map
 let queryContext = "search";
 let sortStyle = "alphabetical";
-let chemicalEntriesCount = 1;
+let chemicalEntriesCount = 0;
 
 
 
 //Button References
-let importBtn = Document.getElementById("import-btn");
-let exportBtn = Document.getElementById("export-btn");
+let importBtn = document.getElementById("import-btn");
+let exportBtn = document.getElementById("export-btn");
 
-let searchContextBtn = Document.getElementById("search-context-btn");
-let addContextBtn = Document.getElementById("add-context-btn");
-let removeContextBtn = Document.getElementById("remove-context-btn");
-let ingredientInputName = Document.getElementById("ingredient-name-input");
 
-let addChemEntryBtn = Document.getElementById("add-chem-entry-btn");
-let removeChemEntryBtn = Document.getElementById("remove-chem-entry-btn");
+let searchContextBtn = document.getElementById("search-context-btn");
+searchContextBtn.addEventListener("click",enterSearchContext);
 
-let submitQueryBtn = Document.getElementById("submit-query-btn");
-let clearQueryBtn = Document.getElementById("clear-query-btn");
+let addContextBtn = document.getElementById("add-context-btn");
+addContextBtn.addEventListener("click", enterAddContext);
 
-let sortDisplayBtn = Document.getElementById("sort-display-btn");
-let clearDisplayBtn = Document.getElementById("clear-display-btn");
+let removeContextBtn = document.getElementById("remove-context-btn");
+removeContextBtn.addEventListener("click", enterRemoveContext);
 
-let clearOutputLogBtn = Document.getElementById("clear-output-log-btn");
+let ingredientInputName = document.getElementById("ingredient-name-input");
+
+
+
+let addChemEntryBtn = document.getElementById("add-chem-entry-btn");
+addChemEntryBtn.addEventListener("click",createChemEntryElement);
+
+let removeChemEntryBtn = document.getElementById("remove-chem-entry-btn");
+removeChemEntryBtn.addEventListener("click", removeLastChemEntry);
+
+
+
+let submitQueryBtn = document.getElementById("submit-query-btn");
+let clearQueryBtn = document.getElementById("clear-query-btn");
+
+let sortDisplayBtn = document.getElementById("sort-display-btn");
+let clearDisplayBtn = document.getElementById("clear-display-btn");
+
+
+
+let clearOutputLogBtn = document.getElementById("clear-output-log-btn");
 
 
 
 //Display References
-let totalDatabasePopulationDisplay = Document.getElementById("total-database-population-display");
-let currentTablePopulationDisplay = Document.getElementById("current-table-population-display");
-let displayTableBody = Document.getElementById("display-table-body");
-let logOutputDisplay = Document.getElementById("log-output");
+let totalDatabasePopulationDisplay = document.getElementById("total-database-population-display");
+let currentTablePopulationDisplay = document.getElementById("current-table-population-display");
+let displayTableBody = document.getElementById("display-table-body");
+let logOutputDisplay = document.getElementById("log-output");
 
 
 
@@ -48,6 +64,32 @@ class Ingredient{
 
 
 // Helper Functions
+//Query-context related
+function enterSearchContext(){
+    if (queryContext !== "search"){
+        queryContext= "search";
+        addChemBoundsInputs();
+        removeChemValueInputs();
+    }
+}
+
+function enterAddContext(){
+    if (queryContext !== "add"){
+        queryContext= "add";
+        removeChemBoundsInputs();
+        addChemValueInputs();
+    }
+}
+
+function enterRemoveContext(){
+    if (queryContext !== "remove"){
+        queryContext= "remove";
+        removeChemBoundsInputs();
+        removeChemValueInputs();
+    }
+}
+
+//Chemical-field related
 function buildChemNameInputHTML(chemEntryNumber){
     return `<div class="input-group pb-1 chem-name-container">
         <label class="input-group-text " for="chem${chemEntryNumber}-name">Chem${chemEntryNumber}:</label>
@@ -56,45 +98,42 @@ function buildChemNameInputHTML(chemEntryNumber){
 }
 
 function buildChemBoundsInputHTML(chemEntryNumber){
-    return `<div class="pb-3 chem-bounds-container">
-        <div class="input-group pb-1 min-bound-container">
+    return `<div class="input-group pb-1 min-bound-container">
             <label class="input-group-text" for="chem${chemEntryNumber}-min-bound">Min:</label>
             <input id="chem${chemEntryNumber}-min-bound" class="form-control chem-bound" type="number" placeholder="---" min="1" size="3">
         </div>
         <div class="input-group max-bound-container">
             <label class="input-group-text " for="chem${chemEntryNumber}-max-bound">Max:</label>
             <input id="chem${chemEntryNumber}-max-bound" class="form-control chem-bound" type="number" placeholder="---" min="1" size="3">
-        </div>
-    </div>`;
+        </div>`;
 }
 
 function buildChemValueInputHTML(chemEntryNumber){
-    return `<div class="input-group pb-3 chem-value-container">
-        <label class="input-group-text " for="chem${chemEntryNumber}-value">Qty:</label>
-        <input id="chem${chemEntryNumber}-value" class="form-control chem-value" type="number" placeholder="---" min="1">
-    </div>`;
+    return `<label class="input-group-text " for="chem${chemEntryNumber}-value">Qty:</label>
+        <input id="chem${chemEntryNumber}-value" class="form-control chem-value" type="number" placeholder="---" min="1">`;
 }
 
 function buildChemEntryHTML(newChemEntryNumber){
     if (queryContext === "search"){
-        return `<div id="chem${newChemEntryNumber}-entry" class="chem-field">
-            ${buildChemNameInputHTML(newChemEntryNumber)}
-            ${buildChemBoundsInputHTML(newChemEntryNumber)}
-        </div>`;
+        return `${buildChemNameInputHTML(newChemEntryNumber)}
+            ${buildChemBoundsInputHTML(newChemEntryNumber)}`;
     }
-    else {
-        return `<div id="chem${newChemEntryNumber}-entry" class="chem-field">
-            ${buildChemNameInputHTML(newChemEntryNumber)}
-            ${buildChemValueInputHTML(newChemEntryNumber)}
-        </div>`;
+    else if (queryContext === "add"){
+        return `${buildChemNameInputHTML(newChemEntryNumber)}
+        ${buildChemValueInputHTML(newChemEntryNumber)}`;
+    }
+    else if (queryContext === "remove"){
+        return `${buildChemNameInputHTML(newChemEntryNumber)}`;
     }
 }
 
 function createChemEntryElement(){
     let chemEntryContainer = document.getElementById("chemical-entry-container");
     let newChemEntry = document.createElement("div");
+    newChemEntry.setAttribute("id",`chem${chemicalEntriesCount + 1}-entry`);
+    newChemEntry.setAttribute("class","chem-entry");
     chemEntryContainer.append(newChemEntry);
-    newChemEntry.innerHTML= buildChemEntryHTML(chemicalEntriesCount + 1); // Offset the entry# by 1
+    newChemEntry.innerHTML= buildChemEntryHTML(chemicalEntriesCount + 1); // Offset chem entry# by 1
     chemicalEntriesCount++;
 }
 
@@ -110,19 +149,50 @@ function getChemEntryElement(chemEntryNumber){
 function removeEntry(chemEntryNumber){
     if (doesChemEntryExist(chemEntryNumber)){
         getChemEntryElement(chemEntryNumber).remove();
+        chemicalEntriesCount--;
     }
 }
 
+function removeLastChemEntry(){
+    if (chemicalEntriesCount > 0)
+        removeEntry(chemicalEntriesCount);
+}
+
 function removeChemBoundsInputs(){
-    for (let i = 0; i < chemicalEntriesCount; i++){
-        //chemical entries are displayed with an offset of +1 for readability
-        document.getElementById(`chem${i + 1}-min-bound`).remove();
-        document.getElementById(`chem${i + 1}-max-bound`).remove();
+    let chemBoundsContainers = document.getElementsByClassName("chem-bounds-container");
+    for (let i = 0; i < chemBoundsContainers.length; i++){
+        chemBoundsContainers[i].remove();
+    }
+}
+
+function addChemBoundsInputs(){
+    let entryElements = document.getElementsByClassName("chem-entry");
+    for (let i=0; i < entryElements.length;i++){
+        let newDivElement = document.createElement("div");
+        newDivElement.setAttribute("class","pb-3 chem-bounds-container")
+        newDivElement.innerHTML= buildChemBoundsInputHTML(i+1); //offset chem entry# by +1
+        entryElements[i].append(newDivElement);
+    }
+    
+}
+
+function removeChemValueInputs(){
+    let chemValueContainers = document.getElementsByClassName("chem-value-container");
+    for (let i = 0; i < chemValueContainers.length; i++){
+        chemValueContainers[i].remove();
+    }
+}
+
+function addChemValueInputs(){
+    let entryElements = document.getElementsByClassName("chem-entry");
+    for (let i=0; i < entryElements.length;i++){
+        let newDivElement = document.createElement("div");
+        newDivElement.setAttribute("class","input-group pb-3 chem-value-container")
+        newDivElement.innerHTML= buildChemValueInputHTML(i+1); //offset chem entry# by +1
+        entryElements[i].append(newDivElement);
     }
 }
 
 // Event/Action Functions
-function addChemEntryOnclick(){
-    
-}
+
 
